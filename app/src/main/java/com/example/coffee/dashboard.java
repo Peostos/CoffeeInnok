b77ed7c76b5c40ab056f83ead1f601462c18d4a6
Kira Doma <kolya111197@mail.ru>
2024-03-19 11:01:53 +0900

    bar ne smogli ubrat

---
+++ app/src/main/java/com/example/coffee/dashboard.java
@@ -0,0 +1,22 @@
+package com.example.coffee;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class dashboard extends AppCompatActivity {
+    private Button btn1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+    public void Perexod(View v){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
